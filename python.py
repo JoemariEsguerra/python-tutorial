e03eefd04e6548a30a7e92c9631743352f37d585
Omar Esguerra <joemari1000@gmail.com>
2024-02-05 15:26:50 +0800

    Multiple values

--- python.py
+++ python.py
@@ -1,10 +1,14 @@
 #this is a comment
 print('Hello World!')
+
+
 #python variables
 x = "Kobe Bryant"
 y = 24
 print(x)
 print(y)
+
+
 #Variable Names
 myplayer = "Lebron James"
 MYplayer = "Lebron James"
@@ -18,3 +22,10 @@ print(_myPlayer)
 print(my_PLAYER)
 print(_my_player)
 print(my_player2)
+
+
+#Multiple values
+player1, player2, player3 = "Kobe Bryant", "Michael Jordan","Lebron James"
+print(player1)
+print(player2)
+print(player3)
