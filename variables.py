105206bf757deeb6cf7cfd16d7c991efd1b1d3a4
Omar Esguerra <joemari1000@gmail.com>
2024-02-08 08:43:01 +0800

    variables.py

---
+++ variables.py
@@ -0,0 +1,7 @@
+#conversion variables pounds to kilograms
+print("POUNDS TO KILOGRAMS CONVERSION")
+print("______________________________")
+pounds = input("What is your weight in pounds?: ")
+kg =  0.4535923 * int(pounds)
+print("Your weight in pounds converted to kilograms is: " + str(kg) + " kg.")
+      
